c0f93852d5520249bf0bccda48e3236d3bda5901
ArditCeno <arditceno1@gmail.com>
2026-07-21 12:01:03 +0200

    Added Home Page

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -2,5 +2,22 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage extends BasePage {
-    private By searchBox =
+    private By searchBox = By.id("search-input");
+    private By searchButton = By.cssSelector("button[type='submit']");
+    private By topCategoryMenu = By.xpath("//a[contains(text(),'Men')]");
+    private By subCategoryMenu = By.xpath("//a[contains(text(),'Jackets')]");
+
+    public HomePage (WebDriver driver){
+        super(driver);
+    }
+
+    public void searchProduct(String productName){
+        writeText(searchBox, productName)
+        click (searchButton);
+    }
+    public void navigateToSubcategory(){
+        click (topCategoryMenu);
+        click (subCategoryMenu);
+    }
+
 }
